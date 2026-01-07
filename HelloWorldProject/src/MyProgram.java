f8ce0304f07b78ffd6fee32672281494d34c2d17
Chris Lee <chriswole19@gmail.com>
2026-01-07 17:47:42 +0000

    working

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -1,7 +1,7 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
+ * TODO: Christopher Lee
+ * TODO: 1/6/26
+ * TODO: Pd:6
  * TODO: Program Description
  */
 import java.util.Scanner;
@@ -17,15 +17,39 @@ public class MyProgram {
 		int limitTrackA = 100000, limitTrackB = 100000, limitTrackC = 100000;
 	
 		Scanner x = new Scanner(System.in);
+		Queue<Train> nick0 = new LinkedList<Train>();
 		try{
 			File f = new File("HelloWorldProject/src/data.txt");
 			x = new Scanner (f);
-			String name = x.nextLine();
-			System.out.println(name);
+			while(x.hasNext()){
+				String med = x.nextLine();
+				if(med.indexOf("CAR")!=-1){
+					nick0.add(new Train(med, x.nextLine(), x.nextLine(), x.nextLine(), x.nextInt(), x.nextInt()));
+					x.nextLine();
+				}
+				else{
+					nick0.add(new Train(med, x.nextLine()));
+				}
+			}
 		}
 		catch(Exception e)
 		{
-			System.out.println(e.getMessage());
+			//System.out.println(e.getMessage());
 		}
+		
+		Queue<Train> leo1 = new LinkedList<Train>();
+
+		Queue<Train> A = new LinkedList<Train>();
+		int aWeight =0;
+		Queue<Train> B = new LinkedList<Train>();
+		int bWeight =0;
+		Queue<Train> C = new LinkedList<Train>();
+		int cWeight =0;
+		Queue<Train> D = new LinkedList<Train>();
+		int dWeight =0;
+
+		System.out.println(nick0.remove());
+		System.out.println(nick0.remove());
+		//System.out.println(nick0.remove());
 	}
 }
--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -2,5 +2,48 @@ public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
 
+    public Train (String n, String p, String o, String d, int w, int m){
+        name=n;
+        product=p;
+        origin=o;
+        destination=d;
+        weight=w;
+        miles=m;
+    }
+    public Train (String n, String d){
+        name=n;
+        product="";
+        origin="";
+        destination=d;
+    
+    }
+    public String getName(){
+        return name;
+    }
+    public String getProduct(){
+        return product;
+    }
+    public String getDestination(){
+        return destination;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public int getMiles(){
+        return miles;
+    }
+    public void setMiles(int i){
+        miles = i;
+    }
+    public String toString(){
+        String r =name+product+origin+destination;
+        if(weight!=0){
+            r+=weight;
+        }
+        if(miles!=0){
+            r+=miles;
+        }
+        return r;
+    }
 
 }
