3e147c94e4743a6f0569b7d81b2ce0bdb40f00f0
Chris Lee <chriswole19@gmail.com>
2026-01-08 18:10:30 +0000

    working

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.util.Stack;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class MyProgram {
 	public static int val = 0;
@@ -27,9 +28,8 @@ public class MyProgram {
 					nick0.add(new Train(med, x.nextLine(), x.nextLine(), x.nextLine(), x.nextInt(), x.nextInt()));
 					x.nextLine();
 				}
-				else{
+				else
 					nick0.add(new Train(med, x.nextLine()));
-				}
 			}
 		}
 		catch(Exception e)
@@ -48,8 +48,63 @@ public class MyProgram {
 		Queue<Train> D = new LinkedList<Train>();
 		int dWeight =0;
 
-		System.out.println(nick0.remove());
-		System.out.println(nick0.remove());
 		//System.out.println(nick0.remove());
+		//System.out.println(nick0.remove());
+		//System.out.println(nick0.remove());
+		printMsg(nick0);
+		int i=nick0.size();
+		//fix this
+		for(int e=0;e<i;e++){
+			Train jas = nick0.remove();
+			if(jas.getName().indexOf("CAR")!=-1)
+				if(jas.getMiles()>700)
+					leo1.add(jas);
+				else
+					nick0.add(jas);
+		}
+		printMsg(nick0);
+		while(!leo1.isEmpty()){
+			Train kat = leo1.remove();
+			kat.setMiles(100);
+			nick0.add(kat);
+		}
+		
+		while(!nick0.isEmpty()){
+			Train lim = nick0.remove();
+			if(lim.getDestination().equals("Trenton")){
+				if((aWeight+lim.getWeight()) > limitTrackA)
+					printMsg(A);
+				A.add(lim);
+				aWeight+=lim.getWeight();
+			}else if(lim.getDestination().equals("Charlotte")){
+				if((aWeight+lim.getWeight()) > limitTrackB)
+					printMsg(B);
+				B.add(lim);
+				bWeight+=lim.getWeight();
+			}else if(lim.getDestination().equals("Baltimore")){
+				if((aWeight+lim.getWeight()) > limitTrackC)
+					printMsg(C);
+				C.add(lim);
+				cWeight+=lim.getWeight();
+			}else {
+				D.add(lim);
+				dWeight+=lim.getWeight();
+			}
+			System.out.println("e");
+		}
+		//System.out.println(A.remove());
+		System.out.println(aWeight);
+		System.out.println(bWeight);
+		System.out.println(cWeight);
+		printMsg(A);
+		//printMsg(B);
+		//printMsg(C);
+	}
+	public static void printMsg(Queue<Train> e){
+    	while(!e.isEmpty())
+    	{
+		System.out.println(e.peek().getName()+" contains "+e.peek().getProduct());
+		e.remove();
+    	}//loops through list
 	}
 }
